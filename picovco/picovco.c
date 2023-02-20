a672fd64dc17943d7e3365a25ea1122bd07a1d0e
extrachange <32955678+extrachange@users.noreply.github.com>
2023-02-20 12:27:17 +0000

    minor corections

--- picovco/picovco.c
+++ picovco/picovco.c
@@ -3,7 +3,10 @@
 #include "pico/stdlib.h"
 #include "hardware/adc.h"
 
-#define buffer_size 16
+#define BUFFER_SIZE 16
+#define EXP_FM_PIN 26       //ADC0
+#define PITCH_PIN 27        //ADC1
+#define PW_PIN 27           //ADC2
 
 int state;                  //
 
@@ -11,10 +14,10 @@ struct osc{
         unsigned int sample_rate;
         unsigned int output[5];     //saw, triangle, square, sine, custom
         float counter;              //count from 0 to 10x sample rate
-        float freq_hz;          
+        float freq_hz;              //preset pitch
         float pw;                   //0 ~ 1 pulse width
-        float fmod_lin;
-        float fmod_exp_v;
+        float fmod_lin;             //linear FM
+        float fmod_exp_v;           // V/Oct input
         bool reset;                 //input should be a pulse
         bool syncout;
     };
@@ -33,9 +36,11 @@ struct osc{
         osc->output[4] = 0;         //unused
     }
 
-    short int buffer[buffer_size];
+    
+
+    short int buffer[BUFFER_SIZE];
     short int *p_buffer = buffer;
-    short int *p_buffer_end = &buffer[buffer_size - 1];
+    short int *p_buffer_end = &buffer[BUFFER_SIZE - 1];
     
     short int fetch_buffer(){
         p_buffer++;
@@ -48,6 +53,9 @@ struct osc{
 int main()
 {
     stdio_init_all();
+    adc_init();
+    adc_gpio_init(EXP_FM_PIN);
+
     while(1){
         switch(state){
             case 0:
