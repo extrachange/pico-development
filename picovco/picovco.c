bdb3f1534ef8d06a015a3aba210b1ae41f1486dc
extrachange <32955678+extrachange@users.noreply.github.com>
2023-02-21 07:02:49 +0000

    fixed buffer

--- picovco/picovco.c
+++ picovco/picovco.c
@@ -1,6 +1,7 @@
 //TBD: osc_update
 
 #include <stdio.h>
+#include <stdint.h>
 #include <math.h>
 #include "pico/stdlib.h"
 #include "hardware/adc.h"
@@ -58,7 +59,43 @@ int state;                  //
         input_a.pw = adc_read()/4096;
     }
 
+    void osc_update(struct osc *osc){
+        adc_get();
+        osc->freq_hz = input_a.pitch;
+        //osc->fmod_exp_v = input_a.exp_fm;
+        //osc->pw = input_a.pw;
+    }
 //-----buffer-----
+    uint16_t buffer[BUFFER_SIZE] = {0};
+    uint16_t buffer_state[BUFFER_SIZE] = {0};   //1 for newly written data, 0 for used data
+    uint16_t in_pos = 0;
+    uint16_t out_pos = 0;
+
+    uint16_t buffer_out(){
+        out_pos++;
+        if (out_pos >= BUFFER_SIZE){
+            out_pos = 0;
+        }
+        buffer_state[out_pos] = 0;
+        return(buffer[out_pos]);
+    }
+
+    uint16_t buffer_in(uint16_t data){   //return 1 if not available
+        in_pos++;
+        if(buffer_state[in_pos]){
+            in_pos--;
+            return(1);
+        }
+        if (in_pos >= BUFFER_SIZE){
+            in_pos = 0;
+        }
+        buffer[in_pos] = data;
+        buffer_state[in_pos] = 1;
+            return(0);
+    }
+
+    //-----OLD-----
+    /*
     uint16_t buffer[BUFFER_SIZE];
     uint16_t *p_buffer_out = &buffer[BUFFER_SIZE/2];
     uint16_t *p_buffer_in = buffer;
@@ -79,15 +116,17 @@ int state;                  //
         }else {
             return(1);
         }
-        p_buffer_out++;
+        p_buffer_in++;
         if(p_buffer_in > p_buffer_end){
             p_buffer_in = buffer;
         }
             return(0);
     }
+    */
     //-----audio io-----
     bool audio_put(repeating_timer_t *rt){  //called at 48khz, fetch buffer and feed to dac
-        pwm_set_gpio_level(16, buffer_out());
+        //pwm_set_gpio_level(16, (uint8_t)buffer_out());
+        gpio_put(17, buffer_out());
         return(true);
     }
 
@@ -102,45 +141,36 @@ int main()
     gpio_set_function(16, GPIO_FUNC_PWM);
     uint slice_num = pwm_gpio_to_slice_num(16);
     pwm_config config = pwm_get_default_config();
-    pwm_config_set_clkdiv(&config, 2.f);
-    pwm_set_wrap(slice_num,65535);
+    pwm_config_set_clkdiv(&config, 1.f);
+    pwm_set_wrap(slice_num,255);        //!
     pwm_init(slice_num, &config, true);
+    
+    //-----temporary optput at GP 17
+    gpio_init(17);
+    gpio_set_dir(17, GPIO_OUT);
     
     //-----timer setup-----
     static repeating_timer_t timer_48k;
     add_repeating_timer_us(42, &audio_put, NULL, &timer_48k);   //!!!!now @ 24k for test
     //-----create osc1-----
     struct osc osc1 = {
-        48000, {0,0,0,0,0}, 0, 440, 0.5f, 0, 0, false, true
+        48000, {0,0,0,0,0}, 0, 880, 0.5f, 0, 0, false, true
     };
+    struct osc *p_osc1 = &osc1;
     uint16_t audio_data = 0;
+
     while(1){       //main loop
         
+        //adc_select_input(1);
+        //osc1.freq_hz = adc_read();
+        osc_update(p_osc1);
+
         if(buffer_in(audio_data)){
-
+            //idle
         }else{
-            osc_next(osc1);
-            audio_data = osc1.output[0];
+            osc_next(p_osc1);
+            audio_data = osc1.output[2];
         }
-
-
-        /*
-        switch(state){
-            case 0:
-            
-            break;
-            case 1:
-
-            break;
-            case 2:
-            
-            break;
-            default:
-
-            break;
-        }
-        */
-    }
-    
+    }   
     return 0;
 }
